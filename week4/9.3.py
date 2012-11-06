cfc4eb6348aeff29c75006ae486debf5836a62fe
Peter Van Eenoo <petervaneenoo@gmail.com>
2012-11-05 18:54:40 -0800

    eme

--- week4/9.3.py
+++ week4/9.3.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python2
 
-def fit():
-#iterate through a file. Pass the word off to has_no_e
-	f = open('words.txt', 'r')
-	no_e_count = 0
-	count = 0
-	for line in f:
-		word = line.strip()
-		if has_no_e(word):
-			count = count + 1
-			no_e_count = no_e_count +1
-		else:
-			count = count + 1
-	f.close()
-	#print no_e_count
-	#print count
-	print  100.0 * no_e_count / count
-def has_no_e(word):
-	if 'e' in word:
-		return False
-	else: 
-		return True
+def avoids(word, chars):
+	flag = False
+	for l in chars:
+		flag = flag or l in word
+	return flag
 
+#print avoids('me', 'cetwf')
 
-fit()
+prompt = "input a word. hit enter and enter all of the characters you want to check for\n"
+user_input_a = raw_input(prompt)
+user_input_b = raw_input('and the letters\n')
+
+print avoids(user_input_a,user_input_b)
